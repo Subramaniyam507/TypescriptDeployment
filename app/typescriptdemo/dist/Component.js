e1e53b6e01f5abb1085abe2ff2bcf0bf8a182c8e
Subramaniyam N <subramaniyam96@gmail.com>
2025-11-12 13:05:59 +0000

    commit

---
+++ app/typescriptdemo/dist/Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("com.sub.typescriptdemo.Component",{metadata:{manifest:"json"}})});
+//# sourceMappingURL=Component.js.map
--- srv/testservice.ts
+++ srv/testservice.ts
@@ -8,6 +8,7 @@ module.exports = ((srv:ApplicationService)=>{
 
       srv.on('getOrders',async (req:Request)=>{
            const {Orders} = srv.entities
+           
           let result = await srv.run(SELECT.from(Orders))
           return result
       })
